6055dc99433fb67af7bf0489c3171f903c3f0189
DrummDaddy <alexpospelov87@gmail.com>
2026-02-13 17:57:38 +0700

    create postgres and models

--- app/internal/models/models.go
+++ app/internal/models/models.go
@@ -1,1 +1,24 @@
 package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID           string  `json:"id"`
+	Email        string  `json:"email"`
+	Phone        *string `json:"phone"`
+	PasswordHash string  `json:"-"`
+
+	FirstName string  `json:"first_name"`
+	LastName  string  `json:"last_name"`
+	AvatarUrl *string `json:"avatar_url"`
+
+	IsActive   bool       `json:"is_active"`
+	IsVerified bool       `json:"is_verified"`
+	LastLogin  *time.Time `json:"last_login"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
